58e8c125a70e0c972c841ee4176033a8d4df581a
Jung Hyeon Heo <gjgmlwjd9@gmail.com>
2024-04-08 21:29:17 +0900

    배포에러수정

--- src/app/header/mNav_Compo.js
+++ src/app/header/mNav_Compo.js
@@ -1,7 +1,7 @@
 'use client';
 import React, { useState,useEffect } from 'react';
 import Link from "next/link";
-export default function mNavCompo() {
+export default function MNavCompo() {
   const [visible, setVisible] = useState(false);
   //토글함수
   const toggleVisibility = () => {
